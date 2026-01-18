64e972382b5ed77b9db2e0e178388586aaac623e
vai-sh-kh <vaishakh.p.handysquad.in>
2026-01-18 21:00:33 +0530

    feat: redesigned service listing page

--- src/app/(main)/about-us/layout.tsx
+++ src/app/(main)/about-us/layout.tsx
@@ -11,23 +11,6 @@ export default function AboutUsLayout({
         <SmoothScroll>
             {/* No Header here - about-us page has its own ScrollingNav */}
             <div>{children}</div>
-            <Footer />
-            <Toaster
-                position="top-right"
-                visibleToasts={5}
-                gap={12}
-                expand={true}
-                toastOptions={{
-                    className: "!z-[99999]",
-                    duration: 4000,
-                    style: {
-                        zIndex: 99999,
-                        minWidth: "300px",
-                        maxWidth: "400px",
-                        marginBottom: "12px",
-                    },
-                }}
-            />
         </SmoothScroll>
     );
 }
